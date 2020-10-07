365b55ef548363bee56fdedc5f4d5b5e759e3906
Helberte <helberteads@gmail.com>
2020-10-07 18:05:43 -0400

    Pequenas alterações
    Pequenas alterações

--- SQL/sp_atualiza_leituras.sql
+++ SQL/sp_atualiza_leituras.sql
@@ -30,7 +30,8 @@ begin
 				   t.quantidade_paginas,
 				   t.tipo_de_leitura,
 				   1 [RESULTADO],
-				   @quantidade [QUANTIDADE]  from livro l, leitura t, usuario u
+				   @quantidade [QUANTIDADE],
+				   t.fk_id_Livro  from livro l, leitura t, usuario u
 									Where l.fk_id_usuario = u.id
 									and t.fk_id_Livro = l.id
 									and t.fk_id_usuario = u.id
